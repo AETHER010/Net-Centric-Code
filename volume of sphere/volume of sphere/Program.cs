87d0207ba4ad2f59afee2a87342b390485944bb7
HI MESH <50093191+HIMESH1998@users.noreply.github.com>
2020-09-16 08:21:36 +0545

    Add files via upload

---
+++ volume of sphere/volume of sphere/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Volume_of_sphere
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Volume of sphere  calculation\n");
+            Console.Write("Enter the radius of sphere: ");
+            double r = double.Parse(Console.ReadLine());
+            double a = (4 * Math.PI * r * r * r) / 3;
+            Console.WriteLine($"Volume of sphere is {a }  unit cube.");
+        }
+    }
+}
