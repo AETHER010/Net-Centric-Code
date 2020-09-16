07745c451aad3531202b554d181231ec3fdaaf5d
HI MESH <50093191+HIMESH1998@users.noreply.github.com>
2020-09-16 08:06:05 +0545

    Add files via upload

---
+++ Body mass index/Body mass index/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Body_Mass_Index
+{
+    class Program
+    {
+        static void Main()
+        { 
+           
+            Console.Write("Enter the weight in kg: "); 
+            double w = double.Parse(Console.ReadLine());
+            Console.Write("Enter the height in meter: "); 
+            double h = double.Parse(Console.ReadLine());
+            double bmi = w / (h * h);
+            Console.WriteLine($"Body Mass Index is {  bmi}");
+        }
+    }
+}
